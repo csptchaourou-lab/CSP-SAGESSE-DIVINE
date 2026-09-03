85652572e7fad5422bd341f60660b8b6ae3cab18
csptchaourou-lab <csp.tchaourou@gmail.com>
2026-09-03 17:15:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -722,10 +722,9 @@ function Coquille({ role, roleLabel, onQuitter, children, nav, actif, setActif }
     </div>
   );
 function EspaceFondateur(props) {
-  // Le fondateur = directeur avec tous les droits
-  return EspaceDirecteur({ ...props, isFondateur: true });
+  return <EspaceDirecteur {...props} isFondateur={true} />;
 }
-
+  
 /* ------------------------------------------------------------------
    ESPACE DIRECTEUR
 ------------------------------------------------------------------- */
